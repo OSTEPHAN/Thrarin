9eac2cc1c83407788d71aa5a3a1d0200a515e689
O.STEPHAN <olivier.stephan@live.fr>
2017-05-05 00:48:42 +0200

    Thrarin.Console.Logging

---
+++ Thrarin/Thrarin.Samples.Console/LoggingModule.cs
@@ -0,0 +1,61 @@
+﻿
+
+namespace Thrarin.Console
+{
+    using Autofac;
+    using Microsoft.Extensions.Logging;
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    internal sealed class FactoryLogger : Logging.ILogger
+    {
+        private readonly ILoggerFactory coreFactory;
+        public FactoryLogger(ILoggerFactory coreFactory)
+        {
+            this.coreFactory = coreFactory;
+        }
+
+        private ILogger GetLogger(string location)
+        {
+            var className = location.Split('.').FirstOrDefault();
+            var fullyClassName = string.Format("{0}.{1}", this.GetType().Namespace, className);
+            var classType = Type.GetType(fullyClassName);
+            var logger = this.coreFactory.CreateLogger(classType);
+            return logger as ILogger;
+        }
+
+        void Logging.ILogger.LogError(string message, string location)
+        {
+            this.GetLogger(location).LogError(message);
+        }
+
+        void Logging.ILogger.LogException(System.Exception exception, string location)
+        {
+            this.GetLogger(location).LogCritical(exception.Message);
+        }
+
+        void Logging.ILogger.LogInformation(string message, string location)
+        {
+            this.GetLogger(location).LogInformation(message);
+        }
+
+        void Logging.ILogger.LogTrace(string message, string location)
+        {
+            this.GetLogger(location).LogTrace(message);
+        }
+
+        void Logging.ILogger.LogWarning(string message, string location)
+        {
+            this.GetLogger(location).LogWarning(message);
+        }
+    }
+
+    internal sealed class LoggingModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<FactoryLogger>().As<Logging.ILogger>();
+        }
+    }
+}
---
+++ Thrarin/Thrarin.Samples.Console/Program.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Thrarin.Console
+{
+    using Configuration;
+    using Logging;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using Storage;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+
+            var autofac = new AutofacDependencyResolver() as IDependencyResolver;
+            var loggingServiceProvider = new ServiceCollection().AddLogging().BuildServiceProvider();
+            var loggingFactory = loggingServiceProvider.GetRequiredService<ILoggerFactory>();
+            loggingFactory.AddConsole();
+            autofac.Register(() => loggingFactory);
+            autofac.Configure();
+
+            var entityStore = autofac.Resolve<IEntityStore>();
+            entityStore.Install();
+
+            autofac.Resolve<Logging.ILogger>().LogInformation("Starting ...");
+            System.Console.ReadLine();
+        }
+    }
+}
